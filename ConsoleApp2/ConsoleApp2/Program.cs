10166b11f60ad5aed5c45b4d3019537ac44be78e
BangRin <63942165+BangRin@users.noreply.github.com>
2022-04-11 14:17:50 +0900

    0411 초기커밋
    인풋, 아웃풋 테스트, 진행완료
    리디렉션 테스트 진행중

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace RunProcess
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = @"D:\Lunaserver\log4j\Server.bat",
+                        UseShellExecute = false, 
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = false
+                    }
+                };
+
+                process.Start();
+                int tpsCount = 0;
+                while(!process.StandardOutput.EndOfStream)
+                {
+                    var line = process.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                    if(line.Contains("/테스트 입갤"))
+                        tpsCount++;
+                    else if(tpsCount > 3)
+                    {
+                        Console.WriteLine("테스트 입갤함");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
