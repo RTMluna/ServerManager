4e8ad45bb443cae0a42045d9e5867a94b2567898
BangRin <leeg0520@naver.com>
2022-06-24 16:43:02 +0900

    0624-임시코딩
    FileSystemWatcher 임시완성본
    
    -> 바뀔때마다 파일목록 불러와서
    파일이름(uuid)를 배열에 저장후 json으로 저장 7월 후반가능

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ServerUserData
+{
+    public class Program 
+    {
+        System.IO.FileSystemWatcher fileSystemWatcher;
+        public static void Main(string[] args)
+        {
+            FileSystemWatcherSetting();
+        }
+
+        void FileSystemWatcherSetting()
+        {
+            fileSystemWatcher = new FileSystemWatcher("C:\\Users\\midkees\\Downloads\\산업재해안전체험");
+            fileSystemWatcher.EnableRaisingEvents = true;
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.Filter ="*.*";
+
+
+            fileSystemWatcher.Created = UserUUIDCreatedEvent;
+
+        }
+
+        void UserUUIDCreatedEvent(object sender, System.IO.FIleSystemEventArgs e)
+        {
+            Console.WriteLine($"신규 UUID가 추가되었습니다. {e.ChangeType}");
+        }
+    }
+}
---
+++ ServerUserDataJson/ServerUserDataJson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ServerUserData
+{
+    public class Program
+    {
+        class UUIDList
+        {
+            public List<string> UUIDs;
+        }
+
+
+        static FileSystemWatcher fileSystemWatcher;
+        public static void Main(string[] args)
+        {
+            FileSystemWatcherSetting();
+        }
+
+        static void FileSystemWatcherSetting()
+        {
+            fileSystemWatcher = new FileSystemWatcher("C:\\Users\\midkees\\Downloads\\산업재해안전체험");
+            fileSystemWatcher.EnableRaisingEvents = true;
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.Filter = "*.dat";
+
+
+            fileSystemWatcher.Created += new FileSystemEventHandler((a, e) =>
+            {
+                Console.WriteLine($"신규 UUID가 추가되었습니다. {e.Name}");
+            });
+            new System.Threading.AutoResetEvent(false).WaitOne();
+        }
+
+    }
+}
